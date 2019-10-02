a5e986ea5c7273dfe9472ae60fe9152267180f9f
Luis Verde Arregoitia <luis@liomys.mx>
2019-10-02 01:53:49 -0300

    update deprecated fn in test

--- tests/testthat/test-untangle2.R
+++ tests/testthat/test-untangle2.R
@@ -1,8 +1,11 @@
 context("untangle2")
 
 test_that("target variable is called 'regex'", {
-  df <- data_frame(regex=c("gpA","a","b","gpB","c","c"),
-                   x = c(NA,1,2,NA,3,4))
+  df <- data.frame(regex=c("gpA","a","b","gpB","c","c"),
+                   vals = c(NA,1,2,NA,3,4),stringsAsFactors = FALSE)
+
   expect_warning(
     untangle2(df,regex = "anything",orig = regex,new = new_col))
 })
+
+

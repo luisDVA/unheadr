9e280ecb584eac800468828c77106ec3f656e6d1
Luis Verde Arregoitia <luis@liomys.mx>
2019-10-05 13:11:24 -0300

    added test for untangle2

--- tests/testthat/test-untangle2.R
+++ tests/testthat/test-untangle2.R
@@ -1,11 +1,28 @@
 context("untangle2")
 
 test_that("target variable is called 'regex'", {
-  df <- data.frame(regex=c("gpA","a","b","gpB","c","c"),
-                   vals = c(NA,1,2,NA,3,4),stringsAsFactors = FALSE)
+  df <- data.frame(
+    regex = c("gpA", "a", "b", "gpB", "c", "c"),
+    vals = c(NA, 1, 2, NA, 3, 4), stringsAsFactors = FALSE
+  )
 
   expect_warning(
-    untangle2(df,regex = "anything",orig = regex,new = new_col))
+    untangle2(df, regex = "anything", orig = regex, new = new_col)
+  )
 })
 
-
+test_that("grepl matches produce message", {
+  df <- data.frame(
+    regex = c("gpA", "a", "b", "gpB", "c", "c"),
+    vals = c(NA, 1, 2, NA, 3, 4), stringsAsFactors = FALSE
+  )
+  expect_message(
+    untangle2(df, regex = "^gp", orig = regex, new = new_col)
+  )
+  expect_message(
+    untangle2(df, regex = "^gpA", orig = regex, new = new_col)
+  )
+  expect_message(
+    untangle2(df, regex = "xx", orig = regex, new = new_col)
+  )
+})

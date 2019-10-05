2005d234af53b66af8139c35a5839bd19edb1200
Luis Verde Arregoitia <luis@liomys.mx>
2019-10-05 13:36:46 -0300

    update unwrap for deprecated fns

--- R/unwrap_fn.R
+++ R/unwrap_fn.R
@@ -1,4 +1,4 @@
-#' Unwrap values and clean up NAs used as padding within observational units.
+#' Unwrap values and clean up NAs or empty values used as padding.
 #'
 #' @param df A data frame with wrapped values and an inconsistent number of NA
 #'   values used to as within-group padding.
@@ -13,15 +13,24 @@
 #'
 #' @examples
 #' data(primates2017_wrapped)
-#' #using commas to separate elements
+#' # using commas to separate elements
 #' unwrap_cols(primates2017_wrapped,scientific_name,", ")
+#'
+#' # separating with semicolons
+#' df <- data.frame(ounits=c("A",NA,"B","C","D",NA),
+#'     vals=c(1,2,2,3,1,3))
+#' unwrap_cols(df,ounits,";")
+#'
 #' @export
 unwrap_cols <- function(df, groupingVar, separator) {
   groupingVar <- dplyr::enquo(groupingVar)
-  groupVarFctName <- dplyr::quo_name(groupingVar)
-  
+  groupVarFctName <- rlang::as_name(groupingVar)
+
   dffilled <- tidyr::fill(df, !!groupingVar)
   dffilled_grpd <- dplyr::group_by(dffilled, !!groupVarFctName := forcats::fct_inorder(!!groupingVar))
-  dfsummarised <- dplyr::summarise_all(dffilled_grpd, dplyr::funs(paste(na.omit(.), collapse = separator)))
+  dfsummarised <- dplyr::summarise_all(dffilled_grpd, ~ paste(na.omit(.), collapse = separator))
   dplyr::mutate(dfsummarised, !!groupVarFctName := as.character(forcats::fct_inorder(!!groupingVar)))
 }
+
+
+

c66f9212b12f81909a1dbcf09b57cbc972d88c24
Luis Verde Arregoitia <luis@liomys.mx>
2019-10-23 16:05:47 -0300

    more tests for annotate_mf

--- tests/testthat/test-annotate_mf.R
+++ tests/testthat/test-annotate_mf.R
@@ -5,8 +5,17 @@ test_that("error when there is no formatting", {
 })
 
 test_that("error when spreadsheet has empty or NA values in the header row", {
-  expect_error(annotate_mf("./dog_test_NAvar.xlsx", orig = emptyvar, new = Task_annotated))
+  expect_error(
+    annotate_mf("./dog_testNAvar.xlsx", orig = emptyvar, new = Task_annotated),
+    "Check the spreadsheet for empty values in the header row"
+  )
 })
+
+test_that("error when path does not exist", {
+  expect_error(annotate_mf("./wrongpath.xlsx", orig = emptyvar, new = Task_annotated))
+})
+
+
 
 test_that("all four formatting options are annotated", {
   df_ann <- annotate_mf("./dog_test_f.xlsx", orig = Task, new = Task_annotated)
